ad098f3b916bedec4d2eee4eb318ac93fb59aa16
Gaollg <gaollg@qq.com>
2023-06-05 20:06:02 +0800

    修改样式

--- web/src/pages/components/BuyPeopleTang.tsx
+++ web/src/pages/components/BuyPeopleTang.tsx
@@ -7,8 +7,8 @@ export default function BuyPeopleTang(props: {}) {
   const [value, setValue] = useState<number>(1);
   return (
     <div className="flex flex-col items-center justify-center">
-      <div className="flex flex-row">
-        <div>
+      <div>
+        <div className="flex flex-row">
           <InputNumber
             min={1}
             max={100}
@@ -19,22 +19,21 @@ export default function BuyPeopleTang(props: {}) {
               }
             }}
           />
-          <div className="text-sm text-gray-600">
-            剩余数量: (<span className="text-red-700">5</span>/10000)
-          </div>
+          <Button
+            className="ml-2"
+            type="primary"
+            onClick={() => {
+              // setValue(99);
+              // web3.eth.getAccounts(console.log);
+              Web3Helper.login();
+            }}
+          >
+            购买
+          </Button>
         </div>
-
-        <Button
-          className="ml-2"
-          type="primary"
-          onClick={() => {
-            // setValue(99);
-            // web3.eth.getAccounts(console.log);
-            Web3Helper.login();
-          }}
-        >
-          购买
-        </Button>
+        <div className="text-sm text-gray-600">
+          剩余数量: (<span className="text-red-700">5</span>/10000)
+        </div>
       </div>
     </div>
   );

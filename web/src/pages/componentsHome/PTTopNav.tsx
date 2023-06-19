3e6cbb7bab2aaa51e1ce0e1fbeeec5fcf0751e00
Gaollg <gaollg@qq.com>
2023-06-19 11:41:40 +0800

    暂时先这样吧

--- web/src/pages/componentsHome/PTTopNav.tsx
+++ web/src/pages/componentsHome/PTTopNav.tsx
@@ -1,36 +1,38 @@
 export default function PTTopNav() {
   return (
     <div className="bg-white">
-      <div className="bg-[#e60c14] h-[300px] p-4">
-        <div className="flex flex-row items-center justify-center">
-          <div className="mr-8">
-            <img src="https://static.peopletang.io/tang-dao/logo-tangdao.png" className="h-12 w-full " />
-          </div>
-          <div className="flex flex-row gap-4  items-center justify-center font-medium">
-            <div className="cursor-pointer">MINT</div>
-            <div className="cursor-pointer">BACKGROUND</div>
-            <div className="cursor-pointer">FUTURE</div>
-          </div>
-          <div className="grow"></div>
-          <div className="flex flex-row gap-4 items-center justify-center">
-            <div>T</div>
-            <div>D</div>
-            <div className="bg-black text-[#e60c14] py-2 px-2 rounded-md">Connect Wallet</div>
-          </div>
+      <div className="bg-[#e60c14] p-4 flex flex-row items-center justify-center">
+        <div className="mr-8">
+          <img src="https://static.peopletang.io/tang-dao/logo-tangdao.png" className="h-12 w-full " />
+        </div>
+        <div className="flex flex-row gap-4  items-center justify-center font-medium">
+          <div className="cursor-pointer">MINT</div>
+          <div className="cursor-pointer">BACKGROUND</div>
+          <div className="cursor-pointer">FUTURE</div>
+        </div>
+        <div className="grow"></div>
+        <div className="flex flex-row gap-4 items-center justify-center">
+          <div>T</div>
+          <div>D</div>
+          <div className="bg-black text-[#e60c14] py-2 px-2 rounded-md">Connect Wallet</div>
         </div>
       </div>
-      <div
-        className="h-[32px] -mt-[24px] absolute w-full"
-        style={{
-          backgroundImage: 'url(https://static.peopletang.io/tang-dao/logo-peopletang-long.png)',
-          backgroundSize: 'contain',
-        }}
-      ></div>
 
-      <div className="h-[400px] -mt-[110px] w-full z-10">
+      <div className="flex flex-col">
+        <div className="bg-[#e60c14] h-[100px]"></div>
+        <div
+          className="h-[32px] -mt-[24px] w-full"
+          style={{
+            backgroundImage: 'url(https://static.peopletang.io/tang-dao/logo-peopletang-long.png)',
+            backgroundSize: 'contain',
+          }}
+        ></div>
+      </div>
+
+      <div className="-mt-[110px] w-full z-10 relative">
         <div className="grid grid-cols-2">
           <div className="flex items-center justify-center">
-            <img src="https://static.peopletang.io/tang-dao/logo-tang.png" className="w-full h-full" />
+            <img src="https://static.peopletang.io/tang-dao/logo-tang.png" className="w-full object-cover" />
           </div>
           <div className="flex items-center justify-center">
             <div className="box-border">

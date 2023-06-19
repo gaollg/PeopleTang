031488f84e8f811fed60e0d0a068cb9df0f3cd7f
Gaollg <gaollg@qq.com>
2023-06-19 11:16:35 +0800

    实现中间部分

--- web/src/pages/componentsHome/PTTopNav.tsx
+++ web/src/pages/componentsHome/PTTopNav.tsx
@@ -19,7 +19,39 @@ export default function PTTopNav() {
           </div>
         </div>
       </div>
-      <div className="bg-white h-[600px]"></div>
+      <div
+        className="h-[32px] -mt-[24px] absolute w-full"
+        style={{
+          backgroundImage: 'url(https://static.peopletang.io/tang-dao/logo-peopletang-long.png)',
+          backgroundSize: 'contain',
+        }}
+      ></div>
+
+      <div className="h-[400px] -mt-[110px] w-full z-10">
+        <div className="grid grid-cols-2">
+          <div className="flex items-center justify-center">
+            <img src="https://static.peopletang.io/tang-dao/logo-tang.png" className="w-full h-full" />
+          </div>
+          <div className="flex items-center justify-center">
+            <div className="box-border">
+              <div className="px-12">
+                <img
+                  src="https://static.peopletang.io/tang-dao/nft-flash.gif?a=1"
+                  className="w-full h-full rounded-md"
+                />
+              </div>
+              <div className="text-center py-2">从公元 618 年开始，往上或者往下</div>
+              <div className="px-12">
+                <div className="flex items-center justify-center bg-[#e70101] py-2 rounded-md cursor-pointer">MINT</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-white h-[600px]">
+        <div></div>
+      </div>
     </div>
   );
 }

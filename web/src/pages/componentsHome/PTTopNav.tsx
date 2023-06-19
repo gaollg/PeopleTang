81a18bab8cc0ba640fcdef7163a44ad8abed7634
Gaollg <gaollg@qq.com>
2023-06-19 10:40:53 +0800

    add topnav

--- web/src/pages/componentsHome/PTTopNav.tsx
+++ web/src/pages/componentsHome/PTTopNav.tsx
@@ -3,14 +3,16 @@ export default function PTTopNav() {
     <div>
       <div className="bg-[#e60c14] h-[300px] p-4">
         <div className="flex flex-row items-center justify-center">
-          <div>logo</div>
-          <div className="flex flex-row">
-            <div>MINT</div>
-            <div>BACKGROUND</div>
-            <div>FUTURE</div>
+          <div className="mr-8">
+            <img src="https://static.peopletang.io/tang-dao/logo-tangdao.png" className="h-12 w-full " />
+          </div>
+          <div className="flex flex-row gap-4  items-center justify-center font-medium">
+            <div className="cursor-pointer">MINT</div>
+            <div className="cursor-pointer">BACKGROUND</div>
+            <div className="cursor-pointer">FUTURE</div>
           </div>
           <div className="grow"></div>
-          <div className="flex flex-row items-center justify-center">
+          <div className="flex flex-row gap-4 items-center justify-center">
             <div>T</div>
             <div>D</div>
             <div className="bg-black text-[#e60c14] py-2 px-2 rounded-md">Connect Wallet</div>
